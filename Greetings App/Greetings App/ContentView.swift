7bd9f8afce0388032f98d80bbeb80a9c74cd4712
Omar Assidi <oia05@mail.aub.edu>
2025-04-06 00:49:25 +0300

    initialize project

---
+++ Greetings App/Greetings App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greetings App
+//
+//  Created by Omar Assidi on 06/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greetings App/Greetings App/Greetings_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greetings_AppApp.swift
+//  Greetings App
+//
+//  Created by Omar Assidi on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Greetings_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
